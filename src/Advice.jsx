e4b5bcae2cc6fea6f62690d5344b07f1bcdd8849
phatommobb <nickoluoch1130@gmail.com>
2025-05-22 16:36:59 +0300

    html and css

--- src/Advice.jsx
+++ src/Advice.jsx
@@ -1,13 +1,28 @@
 import Dividor from './assets/images/pattern-divider-desktop.svg?react';
 import Dice from './assets/images/icon-dice.svg?react';
+import { useState } from 'react';
 
 function Advice() {
+  const [advice, setAdvice] = useState('');
+  const [adviceNo, setAdviceNo] = useState('');
+  const fetchAdvice = async () => {
+    try {
+      const response = await fetch('https://api.adviceslip.com/advice');
+      const data = await response.json();
+      setAdviceNo(data.slip.id);
+      setAdvice(data.slip.advice);
+    } catch (error) {
+      console.log('error fetching advice', error);
+    }
+  };
   return (
     <div className="container">
-      <h2>Advice # ""</h2>
-      <p className="advice">Some dummy type of a advice</p>
+      <h2>Advice # {adviceNo && adviceNo}</h2>
+      <p className="quote">
+        {advice ? advice : 'Click the button to generate advices'}
+      </p>
       <Dividor className="dividor" />
-      <div className="dice-container">
+      <div className="dice-container" onClick={fetchAdvice}>
         <Dice className="dice" />
       </div>
     </div>
