506eb7faf4d82ceccdb75a5cb0163c427da877ab
phatommobb <nickoluoch1130@gmail.com>
2025-05-22 16:08:35 +0300

    html and css

--- src/Advice.jsx
+++ src/Advice.jsx
@@ -1,11 +1,15 @@
-import Dividor from '../assets/images/icon-dice.svg?react';
+import Dividor from './assets/images/pattern-divider-desktop.svg?react';
+import Dice from './assets/images/icon-dice.svg?react';
 
 function Advice() {
   return (
     <div className="container">
       <h2>Advice # ""</h2>
       <p className="advice">Some dummy type of a advice</p>
-      <div className="dividor"></div>
+      <Dividor className="dividor" />
+      <div className="dice-container">
+        <Dice className="dice" />
+      </div>
     </div>
   );
 }
